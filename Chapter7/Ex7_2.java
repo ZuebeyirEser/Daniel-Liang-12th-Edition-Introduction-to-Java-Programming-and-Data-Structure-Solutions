ba6fa4281d842d6094ef5d24bed4277a3de79147
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-10-21 20:18:50 +0200

    added solution of 7_2

--- Chapter7/Ex7_2.java
+++ Chapter7/Ex7_2.java
@@ -4,7 +4,25 @@ import java.util.Scanner;
 
 public class Ex7_2 {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter a number for reversing");
+        int num = input.nextInt();
+        int reversed = 0;
+
+        System.out.println("Original Number: " + num);
+
+        // run loop until num becomes 0
+        while(num != 0) {
+
+            // get last digit from num
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+
+            // remove the last digit from num
+            num /= 10;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
     }
 }
 

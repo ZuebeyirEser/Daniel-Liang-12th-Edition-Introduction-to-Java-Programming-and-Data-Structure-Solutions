fb8bfdfc6703e03bc9d12c20984152adb6a3f986
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-10-22 17:32:17 +0200

    added solution of 7_1

---
+++ Chapter7/Ex7_1.java
@@ -0,0 +1,41 @@
+package Chapter7;
+
+import java.util.Scanner;
+
+public class Ex7_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enet number of sutdent");
+
+        int[] scores = new int[input.nextInt()];
+        System.out.println("Enter" + (scores.length + 1) + " scores");
+        // Loop to take 4 inputs
+        for (int i = 0; i < scores.length; i++) {
+            scores[i] = input.nextInt(); // Store each input in the array
+        }
+        int bestScore = bestStudentScores(scores);
+
+        for (int i = 0; i < scores.length; i ++) {
+            int diff = bestScore - scores[i];
+            String grade = switch (diff / 10) {
+                case 0 -> "A";
+                case 1 -> "B";
+                case 2 -> "C";
+                case 3 -> "D";
+                default -> "F";
+
+            };
+            System.out.println("Student " + i + "'s score is " + scores[i] + " and grade is " + grade);
+        }
+    }
+    public static int bestStudentScores(int[] array) {
+        int bestScore = 0;
+        for (int j : array) {
+            if (j > bestScore) {
+                bestScore = j;
+            }
+        }
+        return bestScore;
+    }
+
+}

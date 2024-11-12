ec0c3f98f3787e8dd5455a7df4006197d616dd07
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-12 17:00:38 +0100

    added unfinished 7_8 exercise

---
+++ Chapter7/Ex7_8.java
@@ -0,0 +1,17 @@
+package Chapter7;
+
+import java.util.Scanner;
+
+public class Ex7_8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 10 integer: ");
+        int[] intArray = new int[10];
+
+
+    }
+    public static double average(int[] array) {
+
+    }
+    public static double average(double[] array) {}
+}

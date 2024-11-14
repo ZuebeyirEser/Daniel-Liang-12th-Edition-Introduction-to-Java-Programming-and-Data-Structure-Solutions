ef33ce48e2da916cbd42e4e742ab9b0f1c49223f
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-14 18:29:10 +0100

    added solution of 7_10

---
+++ Chapter7/Ex7_10.java
@@ -0,0 +1,19 @@
+package Chapter7;
+
+public class Ex7_10 {
+    public static void main(String[] args) {
+        double[] myArray = {1, 2, 4.2, -3, 5};
+        System.out.println(indexOfSmallestElement(myArray));
+    }
+    public static int indexOfSmallestElement(double[] array) {
+        double small = array[0];
+        int indexOfSmall = 0;
+        for (int i = 0; i<array.length;i++) {
+            if (array[i] < small) {
+                small = array[i];
+                indexOfSmall = i;
+            }
+        }
+        return indexOfSmall;
+    }
+}

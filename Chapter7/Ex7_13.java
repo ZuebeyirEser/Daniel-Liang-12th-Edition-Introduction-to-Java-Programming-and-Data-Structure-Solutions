6f63219a9aca93b0dd7036f9997152a3f380b68a
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-14 20:10:09 +0100

    added solution of 7_13

---
+++ Chapter7/Ex7_13.java
@@ -0,0 +1,28 @@
+package Chapter7;
+
+public class Ex7_13 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 45; i++) {
+            System.out.printf("%4d", getRandom(1,100,4,8,95,93));
+            if ((i + 1) % 15 == 0) {
+                System.out.println();
+            }
+        }
+
+    }
+    public static int getRandom(int start, int end, int... numbers) {
+        boolean isIncluded;
+        int someRandom;
+        do {
+            someRandom = (int) (Math.random()  * (end - start + 1)) + start ;
+            isIncluded = false;
+            for (int i : numbers) {
+                if (i == someRandom) {
+                    isIncluded = true;
+                    break;
+                }
+            }
+        } while (isIncluded);
+        return someRandom;
+    }
+}

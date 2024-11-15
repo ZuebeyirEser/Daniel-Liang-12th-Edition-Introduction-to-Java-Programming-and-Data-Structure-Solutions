e3d5bca5c3f8c0c1c45b99e74e8f87020e583788
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-15 18:39:17 +0100

    created  solution of 7_15

---
+++ Chapter7/Ex7_15.java
@@ -0,0 +1,16 @@
+package Chapter7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex7_15 {
+    public static void main(String[] args) {
+        int[] myArray = {1,2,3,4,5,5,5,6};
+        System.out.println(eliminateDuplicates(myArray));
+
+
+    }
+    public static int[] eliminateDuplicates(int[] list) {
+    }
+
+}

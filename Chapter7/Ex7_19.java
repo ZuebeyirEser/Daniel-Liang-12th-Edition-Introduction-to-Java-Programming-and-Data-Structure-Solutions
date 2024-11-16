04e59c61f86230607ac1ddf76038964f62f1c1de
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-16 19:34:31 +0100

    fixed the isSorted method

--- Chapter7/Ex7_19.java
+++ Chapter7/Ex7_19.java
@@ -26,12 +26,11 @@ public class Ex7_19 {
 
     }
     public static boolean isSorted(int[] list) {
-        boolean status = false;
         for (int i = 0; i < list.length - 1; i++) {
-            if (list[i]<list[i+1]) {
-                status = true;
+            if (list[i] > list[i+1]) {
+                return false;
             }
         }
-        return status;
+        return true;
     }
 }

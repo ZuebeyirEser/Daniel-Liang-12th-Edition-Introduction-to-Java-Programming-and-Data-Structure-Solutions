8858d904a567e78c9650d14013d67f80756288bf
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-17 17:48:32 +0100

    solved the  chapter 7 exercise 17 solution

--- Chapter7/Ex7_17.java
+++ Chapter7/Ex7_17.java
@@ -22,9 +22,27 @@ public class Ex7_17 {
             System.out.print("Score of " + names[i] + ":" );
             scores[i] = input.nextInt();
         }
+        sortDecreasingOrder(names,scores);
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i] + ": " + scores[i]);
+        }
     }
+    /*This solution is exactly like a bubble sort */
     public static void sortDecreasingOrder(String[] name, int[] score) {
-        //
+        for (int i = 0; i < score.length - 1; i++) {
+            for (int j = 0; j < score.length - i - 1; j++) {
+                if (score[j] > score[j+1]) {
+                    int temp = score[j];
+                    score[j] = score[j+1];
+                    score[j+1] = temp;
 
+                    String tempName = name[j];
+                    name[j] = name[j+1];
+                    name[j+1] = tempName;
+
+                }
+            }
+        }
     }
+
 }

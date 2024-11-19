8566886d493cc140f68efef167454bf2c87a4824
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-19 22:32:48 +0100

    added solution of 7_22

--- Chapter7/Ex7_22.java
+++ Chapter7/Ex7_22.java
@@ -2,6 +2,16 @@ package Chapter7;
 
 public class Ex7_22 {
     public static void main(String[] args) {
-
+        String input = "";
+        for (int i = 0; i < args.length;i++) {
+            input += args[i] + " ";
+        }
+        int uppercaseCount = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                uppercaseCount++;
+            }
+        }
     }
 }

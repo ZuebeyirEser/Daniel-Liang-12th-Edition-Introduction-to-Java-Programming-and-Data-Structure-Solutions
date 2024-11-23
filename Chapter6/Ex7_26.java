d9a7a0364aa9fba028da4ec89708a634ada04f00
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-23 23:12:41 +0100

    added ex 7_26

---
+++ Chapter6/Ex7_26.java
@@ -0,0 +1,40 @@
+package Chapter6;
+
+import java.util.Scanner;
+
+public class Ex7_26 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter list1 size and contents:");
+
+        int listOneLength = input.nextInt();
+        int[] listOne = new int[listOneLength];
+        for (int i = 0; i < listOne.length; i++) {
+            listOne[i] = input.nextInt();
+        }
+
+        System.out.println("Enter list1 size and contents:");
+        int listTwoLength = input.nextInt();
+        int[] listTwo = new int[listTwoLength];
+
+        if (equals(listOne,listTwo)) {
+            System.out.println("Two lists are strictly identical");
+        } else {
+            System.out.println("Two lists are not strictly identical");
+        }
+
+
+
+    }
+    public static boolean equals(int[] list1, int[] list2) {
+        if (list1.length != list2.length) {
+            return false;
+        }
+        for (int i = 0; i < list1.length;i++) {
+            if (list1[i] != list2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Chapter7/Ex7_23.java
@@ -0,0 +1,20 @@
+package Chapter7;
+/*
+* A school has 100 lockers and 100 students.
+* All lockers are closed on the first day of school.
+* As the students enter, the first student, denoted as S1, opens every locker.
+* Then the second student, S2, begins with the second locker, denoted as L2, and closes every other locker.
+* Student S3 begins with the third locker and changes every third locker (closes it if it was open and opens it if it was closed).
+* Student S4 begins with locker L4 and changes every fourth locker.
+* Student S5 starts with L5 and changes every fifth locker, and so on, until student S100 changes L100
+* After all the students have passed through the building and changed the lockers,
+* which lockers are open?
+* Write a program to find your answer and display all open locker numbers separated by exactly one space.
+*
+* */
+
+public class Ex7_23 {
+    public static void main(String[] args) {
+        
+    }
+}

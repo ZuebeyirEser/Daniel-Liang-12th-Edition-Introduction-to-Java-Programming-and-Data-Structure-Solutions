24c3b9f93fdf46762b7e1121765b9dd9ca371f95
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-27 00:04:39 +0100

    fix the  solution of 8_3 and added also 8_2

--- Chapter8/Ex8_2.java
+++ Chapter8/Ex8_2.java
@@ -1,2 +1,29 @@
-package Chapter8;public class Ex8_2 {
+package Chapter8;
+
+import java.util.Scanner;
+
+public class Ex8_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[][] matrix = new double[4][4];
+
+        System.out.println("Enter a 4−by−4 matrix row by row:");
+
+        for (int i = 0; i < 4;i++) {
+            for (int j = 0; j < 4;j++) {
+                matrix[i][j] = input.nextDouble();
+            }
+        }
+        System.out.println("Sum of the elements in the major diagonal is " + sumMajorDiagonal(matrix));
+
+
+    }
+    public static double sumMajorDiagonal(double[][] m) {
+        double sum = 0D;
+        // assuming that each array will be even length
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i][i];
+        }
+        return sum;
+    }
 }
--- Chapter8/Ex8_3.java
+++ Chapter8/Ex8_3.java
@@ -1,2 +1,48 @@
-package Chapter8;public class Ex8_3 {
+package Chapter8;
+
+public class Ex8_3 {
+    public static void main(String[] args) {
+        char[][] answers = {
+                {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+                {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+                {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+                {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}
+        };
+        char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+        int[][] correctCountOfStudents = new int[answers.length][2];
+        for (int i = 0; i < answers.length;i++) {
+            correctCountOfStudents[i][0] = i;
+            correctCountOfStudents[i][1] = correctAnswerCount(answers,keys,i);
+        }
+        increasingOrderbubbleSort(correctCountOfStudents);
+        for (int[] result : correctCountOfStudents) {
+            System.out.println("Student " + result[0] + " got " + result[1] + " correct answers.");
+        }
+
+
+
+    }
+    public static void increasingOrderbubbleSort(int[][] num) {
+        for (int i = 0; i < num.length - 1; i++) {
+            for (int j = 0; j < num.length - i - 1; j++) {
+                if (num[j][1] < num[j + 1][1]) {
+                    int[] temp = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static int correctAnswerCount(char[][] answers,char[] keys,int studentNumber) {
+        int correctCount = 0;
+        for (int j = 0; j < answers[studentNumber].length; j++) {
+            if (answers[studentNumber][j] == keys[j]) correctCount++;
+        }
+
+        return correctCount;
+    }
 }

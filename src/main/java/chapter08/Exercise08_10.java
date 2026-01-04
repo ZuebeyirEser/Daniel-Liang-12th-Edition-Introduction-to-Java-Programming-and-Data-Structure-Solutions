82462868401eb134820d8e540dbfd70696a4d9bd
ZuebeyirEser <zubeyir.eser@icloud.com>
2026-01-04 09:37:04 +0100

    fix: exercise 10 form chapter 8

--- src/main/java/chapter08/Exercise08_10.java
+++ src/main/java/chapter08/Exercise08_10.java
@@ -1,8 +1,13 @@
 package chapter08;
 
+
 public class Exercise08_10 {
     public static void main(String[] args) {
         int[][] randomMatrix = generateRandomMatrix(4);
+        System.out.println("The largest index of row is " + findMaxRow(randomMatrix));
+        
+        System.out.println("The largest index of column is " + findMaxCol(randomMatrix));
+
 
 
     }
@@ -18,20 +23,39 @@ public class Exercise08_10 {
         }
         return randomMatrix;
     }
-    public static void findColRow(int[][] twoDArray) {
+    public static int findMaxCol(int[][] twoDArray) {
         int[] row = new int[twoDArray.length];
 
-        int[] col = new int[twoDArray.length];
-
-        
         for (int i = 0; i < twoDArray.length; i++){
             for (int j = 0; j < twoDArray.length; j++) {
 
-                row[i] = twoDArray[i][j] * Math.pow(10.0,twoDArray.length - j );
-                col[i] = twoDArray[j][i] * Math.pow(10,twoDArray.length - i);
+                row[i] += (int) (twoDArray[i][j]  *  Math.pow(10.0,twoDArray.length - j - 1));
+            }
+            
+        }
+        return indexOfMax(row);
+    } 
+
+    public static int findMaxRow(int[][] twoDArray) {
+        int[] col = new int[twoDArray.length];
+
+        for (int i = 0; i < twoDArray.length; i++){
+            for (int j = 0; j < twoDArray.length; j++) {
+                col[i] += (int) (twoDArray[j][i]  *  Math.pow(10.0,twoDArray.length - j - 1));
             }
         }
-
-
+        return indexOfMax(col);
     } 
-}
+    public static int indexOfMax(int[] array) {
+        int max = array[0];
+        int index = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+ }
